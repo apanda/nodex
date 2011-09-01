861fd417e3b542981191bbb1f1792dc3811512b0
Aurojit Panda <aurojit@gmail.com>
2011-09-01 15:38:28 -0700

    Add burrito and traverse, change test.js to have an unassigned var node, some more stuff

--- analyze.js
+++ analyze.js
@@ -6,8 +6,24 @@ fs.readFile('test.js', 'utf-8', function(err, data) {
         console.error('Failed to open file, err %s', err);
         process.exit(1);
     }
-    console.log(data);
+    //console.log(data);
     burrito(data, function(node) {
-        console.log(node);
+        console.log(node.value);
+        if (node.name === 'var') {
+            // for var node.node[1][0] is the name (with node[0] being the descriptive object, 
+            // while node.node[1][1] is the assignment to the name.
+            //console.log(node.node[1][0]);
+            //console.log(node.node[1][1]);
+            console.log('');
+            console.log('');
+            //console.log(node);
+            var assigned = node.state.node[1][0][1];
+            if (assigned) {
+                console.log(node.state.node[1][0][0] + ' ' + assigned[0].name);
+            }
+            else {
+                console.log(node.state.node[1][0][0] + ' unassigned');
+            }
+        }
     });
 });
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var old = 0;
+var foo;
 var n = 0;
 var lock = 0;
 var x = 0;
